97cd59d6b5a22882215de7f39efd7256398e478f
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-30 16:08:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// script.js
+function removeColor() {
+    const selectElement = document.getElementById("colorSelect");
+    const selectedColor = selectElement.value;
+
+    // Check if a color is selected
+    if (selectedColor) {
+        // Find the index of the selected color in the dropdown list
+        const index = Array.from(selectElement.options).findIndex(option => option.value === selectedColor);
+        
+        // Remove the selected color from the dropdown list
+        if (index !== -1) {
+            selectElement.remove(index);
+        }
+    }
+}
